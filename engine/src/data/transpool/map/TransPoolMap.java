5ab7b73cd0734ed882f7ce03503c6a378312977b
Nadav <nadavsuliman@gmail.com>
2020-04-30 16:46:24 +0300

    Removed graph map.

--- engine/src/data/transpool/map/TransPoolMap.java
+++ engine/src/data/transpool/map/TransPoolMap.java
@@ -15,23 +15,19 @@ public class TransPoolMap {
 
     private int width;
     private int length;
-    private Map<String, List<Stop>> mapGraph;
-    private Map<String, Stop> stopMap;          //Maybe this should be a list.
-    private List<Path> pathList;
+
+    private List<Stop> stopMap = new ArrayList<>();
+    private List<Path> pathList = new ArrayList<>();
+
+    private Set<String> stopNameSet = new HashSet<>();
     private Set<Coordinates> stopCoordinates = new HashSet<>();
 
     public TransPoolMap() {
-        stopMap = new HashMap<>();
-        pathList = new ArrayList<>();
         width = MIN_MAP_SIZE;
         length = MIN_MAP_SIZE;
     }
 
     public TransPoolMap(MapDescriptor JAXBMap) throws TransPoolDataException {
-        stopMap = new HashMap<>();
-        pathList = new ArrayList<>();
-        mapGraph = new HashMap<>();
-
         setWidth(JAXBMap.getMapBoundries().getWidth());
         setLength(JAXBMap.getMapBoundries().getLength());
 
@@ -45,23 +41,6 @@ public class TransPoolMap {
             addPath(new Path(JAXBPath));
         }
 
-        for (Map.Entry<String, Stop> stringStopEntry : stopMap.entrySet()) {
-            mapGraph.put(stringStopEntry.getKey(), new ArrayList<>());
-        }
-        initMapGraph();
-
-    }
-
-    public void initMapGraph() throws StopNotFoundException {
-        for (Path path : pathList) {
-            Stop sourceStop = getStop(path.getSource());
-            Stop destinationStop = getStop(path.getDestination());
-
-            mapGraph.get(path.getSource()).add(destinationStop);
-            if (!path.isOneWay()) {
-                mapGraph.get(path.getDestination()).add(sourceStop);
-            }
-        }
     }
 
     public int getWidth() {
@@ -94,20 +73,20 @@ public class TransPoolMap {
         return pathList;
     }
 
-    public Map<String, List<Stop>> getMapGraph() {
-        return mapGraph;
-    }
-
     public Stop getStop(String stopName) throws StopNotFoundException {
-        Stop foundStop = stopMap.get(stopName);
-        if (foundStop == null) {
+        if (!stopNameSet.contains(stopName)) {
             throw new StopNotFoundException(stopName);
         }
-        return foundStop;
+
+        return stopMap
+                .stream()
+                .filter(s -> s.getName().equals(stopName))
+                .findAny()
+                .orElse(null);
     }
 
     private void addStop(Stop stop) throws StopOutOfBoundsException, StopDuplicationException {
-        if (stopMap.containsKey(stop.getName())) {
+        if (!stopNameSet.add(stop.getName())) {
             throw new StopDuplicationException();
         }
         if (!stopCoordinates.add(stop.getCoordinates())) {
@@ -116,28 +95,33 @@ public class TransPoolMap {
         if (!stop.isInBoundsOf(width, length)) {
             throw new StopOutOfBoundsException();
         }
-        stopMap.put(stop.getName(), stop);
+        stopMap.add(stop);
     }
 
     private void addPath(Path path) throws StopNotFoundException {
         String destinationName = path.getDestination();
         String sourceName = path.getSource();
-        if (!stopMap.containsKey(destinationName)) {
+        if (!stopNameSet.contains(destinationName)) {
             throw new StopNotFoundException(destinationName);
         }
-        if (!stopMap.containsKey(sourceName)) {
+        if (!stopNameSet.contains(sourceName)) {
             throw new StopNotFoundException(sourceName);
         }
         pathList.add(path);
+        if (!path.isOneWay()) {
+            pathList.add(
+                    new Path(path.getDestination(), path.getSource(), path.getLength(),
+                            path.isOneWay(), path.getFuelConsumption(), path.getSpeedLimit())
+            );
+        }
     }
 
     public boolean doesPathExist(String source, String destination) {
-        List<Stop> sourcePaths = mapGraph.get(source);
-        for (Stop stop : sourcePaths) {
-            if (destination.equals(stop.getName())) {
-                return true;
-            }
-        }
-        return false;
+        Path foundPath = pathList
+                .stream()
+                .filter(path -> path.getSource().equals(source) && path.getDestination().equals(destination))
+                .findAny()
+                .orElse(null);
+        return foundPath != null;
     }
 }
